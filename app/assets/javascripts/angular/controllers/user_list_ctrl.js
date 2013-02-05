7b7d2bb5300a128627915b0587021312ab9bdeb9
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-02-05 14:33:19 -0800

    compensate for rooted json

--- app/assets/javascripts/angular/controllers/user_list_ctrl.js
+++ app/assets/javascripts/angular/controllers/user_list_ctrl.js
@@ -1,6 +1,7 @@
 function UserListCtrl($scope, $http) {
   $http.get('/users.json').success(function(data) {
-    $scope.users = data;
+    //$scope.users = data; change for using a rooted element
+    $scope.users = data.users;
   });
   $scope.orderProp = 'age';
 }
