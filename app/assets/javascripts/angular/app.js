2c5ca003f8f7994edefeac7cc29474661728c249
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-02-05 15:12:25 -0800

    use angular routing

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -3,3 +3,10 @@
 //= require_tree ./filters
 //= require_tree ./services
 //= require_tree ./widgets
+
+angular.module('app', []).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/users', { templateUrl: 'templates/angular/users-list.html', controller: UserListCtrl }).
+      otherwise({ redirectTo: '/users' });
+}]);
